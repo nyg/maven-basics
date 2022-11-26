a05904d14219b2418aed7a84bb094815ba47b41e
nyg <nyg@users.noreply.github.com>
2022-11-26 23:08:13 +0100

    Import project

---
+++ src/main/java/edu/self/nyg/maven/basics/Main.java
@@ -0,0 +1,13 @@
+package edu.self.nyg.maven.basics;
+
+public class Main {
+
+    /**
+     * This is the main method of my application.
+     *
+     * @param args an unused parameter
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello, friend.");
+    }
+}
