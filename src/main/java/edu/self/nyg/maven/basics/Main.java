7853435ce0cccd6bd8cd4cf661b629956e1e1479
nyg <nyg@users.noreply.github.com>
2023-01-04 22:45:34 +0100

    Add Javadoc

--- src/main/java/edu/self/nyg/maven/basics/Main.java
+++ src/main/java/edu/self/nyg/maven/basics/Main.java
@@ -1,13 +1,25 @@
 package edu.self.nyg.maven.basics;
 
+/**
+ * The only class of the application.
+ *
+ * @author nyg
+ */
 public class Main {
 
     /**
-     * This is the main method of my application.
+     * The entry-point of the application.
      *
      * @param args an unused parameter
      */
     public static void main(String[] args) {
         System.out.println("Hello, friend.");
+    }
+
+    /**
+     * A Javadoc comment to remove a maven-javadoc-plugin warning.
+     */
+    public Main() {
+        // NOP
     }
 }
